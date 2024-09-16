89f3004149e699bf6182f5ce1ae75aa09fac580a
dooodle74 <dpwang01@gmail.com>
2024-09-16 19:08:43 -0400

    first

---
+++ search.py
@@ -0,0 +1,278 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+import util
+from util import Stack, Queue, PriorityQueue
+from game import Directions
+from game import Actions
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+
+def constructPath(map, start, goal):
+    """
+    Returns the path of coordinates using map 
+    """
+    path = []
+    current = goal
+
+    while current != start:
+        path.append(current)
+        current = map[current]
+
+    path.append(start)
+    path.reverse()
+    return path
+
+def constructDirections(path):
+    """
+    Returns NESW directions from coordinate path 
+    """
+    if len(path) < 2:
+        return []
+    
+    directions = []
+
+    for i in range(1, len(path)):
+        print(path)
+        curr = path[i]
+        prev = path[i-1]
+        print(type(curr))
+        print(curr)
+        vector = (curr[0] - prev[0], curr[1] - prev[1])
+        directions.append(Actions.vectorToDirection(vector))
+    
+    return directions
+
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print("Start:", problem.getStartState())
+    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+    """
+    "*** YOUR CODE HERE ***"
+
+    stack = Stack()
+    visited = set()
+    map = {}
+
+    start_state = problem.getStartState()
+    stack.push(start_state)
+
+    while not stack.isEmpty():
+        parent = stack.pop()
+
+        if parent not in visited:
+
+            if problem.isGoalState(parent):
+                return constructDirections(constructPath(map, start_state, parent))
+            
+            visited.add(parent)
+            children = problem.getSuccessors(parent)
+            
+            for child_full in children:
+                child = child_full[0]
+
+                if child not in visited:
+                    stack.push(child)
+                    map[child] = parent
+    return []
+
+def breadthFirstSearch(problem):
+    """Search the shallowest nodes in the search tree first."""
+
+    queue = Queue() 
+    visited = set()
+    map = {}
+
+    start_state = problem.getStartState()
+    queue.push(start_state)
+
+    while not queue.isEmpty():
+        parent = queue.pop()
+
+        if parent not in visited:
+
+            if problem.isGoalState(parent):
+                return constructDirections(constructPath(map, start_state, parent))
+
+            visited.add(parent)
+            children = problem.getSuccessors(parent)
+
+            for child_full in children:
+                child = child_full[0]
+
+                if child not in visited:  
+                    queue.push(child)
+                    map[child] = parent
+
+    return []
+
+
+def uniformCostSearch(problem):
+    """Search the node of least total cost first."""
+        
+    pq = PriorityQueue()
+    visited = set()
+    cost_map = {}
+    path_map = {}
+
+    start_state = problem.getStartState()
+    pq.push(start_state, 0)  
+    cost_map[start_state] = 0
+
+    while not pq.isEmpty():
+        parent = pq.pop()
+        parent_cost = cost_map[parent]  
+
+        if parent in visited:
+            continue
+
+        if problem.isGoalState(parent):
+            return constructDirections(constructPath(path_map, start_state, parent))
+
+        visited.add(parent)
+
+        children = problem.getSuccessors(parent)
+        for child_full in children:
+            child = child_full[0]
+            child_cost = child_full[2]
+
+            new_cost = parent_cost + child_cost
+
+            if child not in visited:
+                if child not in cost_map or new_cost < cost_map[child]:
+                    cost_map[child] = new_cost
+                    path_map[child] = parent
+                    pq.update(child, new_cost) 
+
+    return []
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+
+    pq = PriorityQueue()
+    visited = set()
+    cost_map = {}
+    path_map = {}
+
+    start_state = problem.getStartState()
+    pq.push(start_state, 0)  # Push start state with a priority of f(n) = g(n) + h(n)
+    cost_map[start_state] = 0
+
+    while not pq.isEmpty():
+        parent = pq.pop()
+        parent_cost = cost_map[parent]  # g(n), the actual cost to reach this node
+
+        if parent in visited:
+            continue
+
+        if problem.isGoalState(parent):
+            return constructDirections(constructPath(path_map, start_state, parent))
+
+        visited.add(parent)
+
+        children = problem.getSuccessors(parent)
+        for child_full in children:
+            child = child_full[0]
+            child_cost = child_full[2]
+
+            new_cost = parent_cost + child_cost  # g(n) + cost(child)
+            heuristic_cost = heuristic(child, problem)  # h(n), the heuristic estimate
+            total_cost = new_cost + heuristic_cost  # f(n) = g(n) + h(n)
+
+            if child not in visited:
+                if child not in cost_map or new_cost < cost_map[child]:
+                    cost_map[child] = new_cost
+                    path_map[child] = parent
+                    pq.update(child, total_cost)  # Priority is based on f(n) = g(n) + h(n)
+
+    return []
+
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
